3132ef0cbd6ec87002948a1c3893ad97ac59604f
junyinghu <hujy11@gmail.com>
2017-10-06 11:04:12 +0800

    updated

--- src/utiliDoc/dropDownList.py
+++ src/utiliDoc/dropDownList.py
@@ -14,7 +14,7 @@ class dropDownList(object):
     def getdopdownlist(self, section, id):
         option = self.driver.find_element_by_id(self.config.get(section, id))
         single_options_list = option.find_elements_by_tag_name('option')
-        with open('resource/Time_zone_option.csv', 'w') as fout:
+        with open('resource/dropdown_'+id+'.csv', 'w') as fout:
             fout.write('"","","","Index","Options_list"\n')
             for i, x in enumerate(single_options_list):
                 fout.write('"","","","{}","{}"\n'.format(i, x.get_attribute("innerHTML").encode('utf-8')))
@@ -37,25 +37,30 @@ class dropDownList(object):
         time.sleep(10)
         language = Select(self.driver.find_element_by_id(self.config.get('Board', 'location_board_drop_id_language')))
         language.select_by_index(i)
+        language_elem = language.first_selected_option
         board_style = Select(
             self.driver.find_element_by_id(self.config.get('Board', 'location_board_drop_id_style')))
         board_style.select_by_index(j)
+        board_style_elem = board_style.first_selected_option
         time_zone = Select(
             self.driver.find_element_by_id(self.config.get('Board', 'location_board_drop_id_timezone')))
         time_zone.select_by_index(p)
+        time_zone_elem = time_zone.first_selected_option
         dateformat = Select(
             self.driver.find_element_by_id(self.config.get('Board', 'location_board_drop_id_dateformat')))
         dateformat.select_by_index(z)
+        dateformat_elem = dateformat.first_selected_option
         if z == 7:
             input_date = self.driver.find_element_by_id(
                 self.config.get('Board', 'location_board_drop_id_dateformat_input'))
             input_date.clear()
             input_date.send_keys("12/10/2017")
             # print "Custom 12/10/2017"
+            print i, language_elem.text, j, board_style_elem.text, p, time_zone_elem.text, z, 'Custom 12/10/2017'
+        else:
+            print i, language_elem.text, j, board_style_elem.text, p, time_zone_elem.text, z, dateformat_elem.text
         submit = self.driver.find_element_by_name(self.config.get('Board', 'location_board_btn_name_submit'))
         submit.click()
-        print i, j, p, z
-        # selected_option_element = time_zone.first_selected_option
         # print selected_option_element.text
 
     def selectdropList(self):
